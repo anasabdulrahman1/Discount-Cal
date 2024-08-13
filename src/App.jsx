0152f382cdf12650b2d12324e55876771b607081
anasabdulrahman1 <anas777va@gmail.com>
2024-08-13 13:02:56 +0530

    initial

---
+++ src/App.jsx
@@ -0,0 +1,62 @@
+import React, { useState } from 'react';
+import { Container, Form, Button, Row, Col } from 'react-bootstrap';
+import './App.css'
+
+function DiscountCalculator() {
+  const [originalPrice, setOriginalPrice] = useState('');
+  const [discountPercentage, setDiscountPercentage] = useState('');
+  const [finalPrice, setFinalPrice] = useState(null);
+
+  const calculateDiscount = () => {
+    const discountAmount = (originalPrice * discountPercentage) / 100;
+    setFinalPrice((originalPrice - discountAmount).toFixed(2));
+  };
+
+  const resetCalculator = () => {
+    setOriginalPrice('');
+    setDiscountPercentage('');
+    setFinalPrice(null);
+  };
+
+  return (
+    <Container className="discount-calculator">
+      <h2>Discount Calculator</h2>
+      <Form>
+        <Form.Group controlId="originalPrice">
+          <Form.Label>Amount</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter Amount"
+            value={originalPrice}
+            onChange={(e) => setOriginalPrice(e.target.value)}
+          />
+        </Form.Group>
+
+        <Form.Group controlId="discountPercentage" className="mt-3">
+          <Form.Label>Discount %</Form.Label>
+          <Form.Control
+            type="number"
+            placeholder="Enter % (percentage)"
+            value={discountPercentage}
+            onChange={(e) => setDiscountPercentage(e.target.value)}
+          />
+        </Form.Group>
+
+        <div className="buttons">
+          <Button variant="btn btn-success" onClick={calculateDiscount}>Calculate</Button>
+          <Button variant="btn btn-outline-danger" onClick={resetCalculator}>Reset</Button>
+        </div>
+      </Form>
+
+      {finalPrice && (
+        <Row className="mt-4">
+          <Col>
+            <h4 className="final-price">Discount Amount: &#8377;{finalPrice}</h4>
+          </Col>
+        </Row>
+      )}
+    </Container>
+  );
+}
+
+export default DiscountCalculator;
